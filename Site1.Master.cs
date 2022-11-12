23daafa5da1d866eec85a9620b397baaca65469a
Sebastian Chaves <sebas04@estudiantec.cr>
2022-11-11 22:35:35 -0600

    AvanceLoginAdmin
    -Se crea el LoginAdmin
    
    -Se crean Session para detectar cuando esta en un usuario

--- Site1.Master.cs
+++ Site1.Master.cs
@@ -11,7 +11,26 @@ namespace indioSupermercado
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                if (Session["role"].Equals(""))
+                {
+                    userLoginLink.Visible = true; 
+                    userSignupLink.Visible = true;
+                    LinkButton3.Visible = false;
+                }
+                else if (Session["role"].Equals("ActiveAdmin"))
+                {
+                    userLoginLink.Visible = false;
+                    userSignupLink.Visible = false;
+                    LinkButton3.Visible = true;
+                }
 
+            }
+            catch (Exception ex)    
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script.");
+            }   
         }
 
         protected void LinkButton1_Click(object sender, EventArgs e)
@@ -27,6 +46,17 @@ namespace indioSupermercado
         protected void userSignupLink_Click(object sender, EventArgs e)
         {
             Response.Redirect("userSignup.aspx");
+        }
+
+        protected void LinkButton6_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminLogin.aspx");
+        }
+
+        protected void LinkButton3_Click(object sender, EventArgs e)
+        {
+            Session["role"] = "";
+            Response.Write("<script LANGUAGE='JavaScript' >alert('Logout Sucessful');window.location='homePage.aspx';</script>");
         }
     }
 }
